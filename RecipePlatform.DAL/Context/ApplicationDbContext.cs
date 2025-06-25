0655df7fa48c26830e6b6054f7fe5a9ac6e3d292
Omar al balushi <omar-albalushi@outlook.com>
2025-06-25 13:04:06 +0400

    ِApplication DB Context Done

---
+++ RecipePlatform.DAL/Context/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipePlatform.DAL.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+        public DbSet<Model.Entity.Category> Categories { get; set; }
+        public DbSet<Model.Entity.Recipe> Recipes { get; set; }
+        public DbSet<Model.Entity.Rating> Ratings { get; set; }
+        public DbSet<Model.Entity.User> Users { get; set; }
+    }
+}
