c54869a471d109e05ba971166a6a6e20171f8465
Omar al balushi <omar-albalushi@outlook.com>
2025-06-26 21:37:56 +0400

    Configurations

---
+++ RecipePlatform.DAL/Configuration/RecipeConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipePlatform.DAL.Configuration
+{
+    public class RecipeConfiguration
+    {
+    }
+}
